8adb7d8e5fb523ce19873718e5946abdd4611179
chirodip98 <chirodiplodh@gmail.com>
2022-04-18 10:18:44 +0530

    PascalTriangle Leetcode 118.

---
+++ Day 1/PascalTriangle.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class PascalTriangle {
+
+    public static List<List<Integer>> generate(int numRows) {
+
+        int currRow=0;
+        List<List<Integer>> Matrix = new ArrayList<>();
+
+        while(currRow<numRows)
+        {
+            int NumElements = currRow+1;
+            List<Integer> thisRow = new ArrayList<>();
+
+            Matrix.add(thisRow);
+
+            //Taking care of 0th element
+
+            for(int j=0;j<NumElements;j++)
+            {
+                if(j==0 || j==NumElements-1) thisRow.add(1);
+
+                else
+                {
+                    int prevRow = currRow <= 1 ? currRow : currRow-1;
+                    int newElement = Matrix.get(currRow-1).get(j-1) + Matrix.get(currRow-1).get(j);
+                    thisRow.add(newElement);
+                }
+            }
+
+            currRow++;
+        }
+        for(int i=0;i<Matrix.size();i++)
+        {
+            for(int j=0;j<Matrix.get(i).size();j++)
+            {
+                System.out.print(Matrix.get(i).get(j)+" ");
+            }
+
+            System.out.println();
+        }
+        return Matrix;
+    }
+    public static void main(String[] args) {
+
+        generate(9);
+    }
+}

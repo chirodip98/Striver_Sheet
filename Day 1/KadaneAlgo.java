68e7e9ef925d0a225bfdad1641d8b7190285a3ca
chirodip98 <chirodiplodh@gmail.com>
2022-04-18 11:25:54 +0530

    Kadane's Algorithm.

--- Day 1/KadaneAlgo.java
+++ Day 1/KadaneAlgo.java
@@ -1,28 +1,35 @@
 import java.util.*;
 public class KadaneAlgo {
-    private static int Kandane(int[] arr) {
+    private static List<Integer> Kandane(int[] arr) {
 
-        int max_seen_so_far=Integer.MIN_VALUE;
+        List<Integer> ans = new ArrayList<>();
+        int max_seen_so_far=arr[0];
         int max_end_here=0;
+        int start=0;
         for(int i=0;i<arr.length;i++)
         {
             max_end_here+=arr[i];
             if(max_end_here >max_seen_so_far)
             {
+                ans.clear();
                 max_seen_so_far=max_end_here;
+                int k=start;
+                while(k<=i)
+                    ans.add(arr[k++]);
+
             }
             if(max_end_here<0)
             {
                 max_end_here=0;
+                start=i+1;
             }
         }
-
-        return max_seen_so_far;
+        System.out.println("The maximum sum obtained: "+max_seen_so_far);
+        return ans;
     }
     public static void main(String[] args) {
-        int arr[]={-2,1,-3,4,-1,2,1,-5,4};
-        int x = Kandane(arr);
-        System.out.println("The maximum sum obtained: "+x);
+        int arr[]={-2,1,-3,4,-1,2,1,5,4};
+        System.out.println(Kandane(arr).toString());
     }
 
 
